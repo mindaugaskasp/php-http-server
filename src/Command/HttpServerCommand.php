ea3408c757b7d181b301a50f8ca6caf3603a7d30
Mindaugas Kasparavičius <kasp.mindaugas@gmail.com>
2024-01-07 22:19:45 +0200

    update command options, pass document root

--- src/Command/HttpServerCommand.php
+++ src/Command/HttpServerCommand.php
@@ -4,26 +4,44 @@ namespace App\Command;
 
 use App\Server\Http\HttpServer;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class HttpServerCommand extends Command
 {
-    public const COMMAND_NAME = 'http-sercer';
+    // todo:
+    // add response factory
+    // static date response factory should serve .html static files
+    // dynamic response factory
+    // dynamic response factory should load modules for every language
+    // each module will know how to parse and retrieve response message from index file, like index.php
+    public const COMMAND_NAME = 'http-server';
+
+    private const OPTION_HOST = 'host';
+    private const ARG_DOC_ROOT = 'document_root';
 
     protected function configure(): void
     {
         $this
             ->setName(self::COMMAND_NAME)
             ->setDescription('Starts the HTTP server')
-            ->addOption('host', null, InputOption::VALUE_OPTIONAL, 'Specify the host (default: 127.0.0.1:8000)', '127.0.0.1:8000')
+            ->addArgument(self::ARG_DOC_ROOT, InputArgument::OPTIONAL, 'HTML/PHP document root file path', './index.html')
+            ->addOption(self::OPTION_HOST, null, InputOption::VALUE_OPTIONAL, 'Specify the host (default: 127.0.0.1:8000)', '127.0.0.1:8000')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $host = $input->getOption('host');
+        $host = $input->getOption(self::OPTION_HOST);
+        $documentRoot = $input->getArgument(self::ARG_DOC_ROOT);
+
+        if (false === str_contains($host, ':')) {
+            $output->writeln('Host must contain port separated by colon (:)');
+
+            return Command::FAILURE;
+        }
 
         $output->writeln("Starting HTTP server on {$host}");
 
@@ -31,6 +49,6 @@ class HttpServerCommand extends Command
             ...explode(':', $host)
         );
 
-        $server->handleRequests();
+        $server->handleRequests($documentRoot);
     }
 }
--- src/Server/Http/HttpServer.php
+++ src/Server/Http/HttpServer.php
@@ -6,6 +6,7 @@ namespace App\Server\Http;
 
 use App\Connection\Socket\Tcp\TcpSocketConnection;
 use App\Server\HttpServerHandler;
+use Exception;
 
 class HttpServer implements HttpServerHandler
 {
@@ -16,7 +17,7 @@ class HttpServer implements HttpServerHandler
         $this->connection = new TcpSocketConnection($address, $port);
     }
 
-    public function handleRequests(): void
+    public function handleRequests(string $documentRootPath): void
     {
         $this->listen();
 
@@ -29,25 +30,28 @@ class HttpServer implements HttpServerHandler
                 // todo: replace this with logger for cli/log file
                 echo sprintf('%s: Request received [%s]', time(), trim($data));
 
-                $response = $this->getResponse();
+                $response = $this->getResponse($documentRootPath);
 
                 $this->connection->write($connection, $response, strlen($response));
-                $this->connection->close($connection);
-            } catch (\Exception $e) {
+            } catch (Exception $e) {
                 echo $e->getMessage();
+            } finally {
+                $this->connection->close($connection);
             }
         }
     }
 
     // todo: mark this method as abstract in separate class
-    public function getResponse(): string
+    public function getResponse(string $documentRoot): string
     {
+        $data = file_get_contents(realpath("./example/index.html"));
+
         // todo remove this later and fetch response content dynamically
         $response = "HTTP/2.0 200 OK\r\n";
         $response .= "Content-Type: text/html\r\n";
         $response .= "Host: 127.0.0.1\r\n";
         $response .= "Connection: Close\r\n\r\n";
-        $response .= "<h1>hello,world</h1>\r\n";
+        $response .= "$data\r\n";
 
         return $response;
     }
